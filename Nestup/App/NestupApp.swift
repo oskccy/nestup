bee9fbc8f717e91c5d4a240c20f050c5d00d785c
Oscar Spencer <oscarspencer@Oscars-MacBook-Air.local>
2023-08-07 20:52:22 -0600

    initial commit
    * raw application

---
+++ Nestup/App/NestupApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestupApp.swift
+//  Nestup
+//
+//  Created by Oscar Spencer on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct NestupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
